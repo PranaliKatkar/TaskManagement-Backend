52c3ee8df4bdf447fca99c9172c8fa0892304f79
Pranali Katkar <pranalikatkar000@gmail.com>
2026-01-11 22:20:49 +0530

    check functionality

--- src/main/java/com/Verzat/VerzatTechno/Scheduler/AlertScheduler.java
+++ src/main/java/com/Verzat/VerzatTechno/Scheduler/AlertScheduler.java
@@ -21,7 +21,7 @@ public class AlertScheduler {
     @Autowired
     private AlertService alertService;
 
-    @Scheduled(cron = "0 25 14 * * ?", zone = "Asia/Kolkata")
+    @Scheduled(cron = "0 27 22 * * ?", zone = "Asia/Kolkata")
     public void runDailyAlertJob() {
         LocalDate today = LocalDate.now(ZoneId.of("Asia/Kolkata"));
         List<Task> allTasks = taskRepo.findAll();
