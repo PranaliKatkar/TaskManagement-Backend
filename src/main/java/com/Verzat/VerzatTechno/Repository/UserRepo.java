348a811b688c21b2ed26e765180829360e783de2
Pranali Katkar <pranalikatkar000@gmail.com>
2026-01-11 08:15:42 +0530

    change AlertController

--- src/main/java/com/Verzat/VerzatTechno/Repository/UserRepo.java
+++ src/main/java/com/Verzat/VerzatTechno/Repository/UserRepo.java
@@ -15,7 +15,7 @@ public interface UserRepo extends JpaRepository<User, Long> {
 
 	Optional<User> findByUsername(String username);
 
-	Optional<User> findByPhoneNumber(String formattedNumber);
+	Optional<User> findByContactNumber(String formattedNumber);
 
 	List<User> findByAlertsEnabledTrue();
 }
